a5551b5540dba8382ce4c915c567d40207220ad6
John Khachian <jaksawesome10@gmail.com>
2023-02-14 19:14:44 -0500

    environment variable setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
+require("dotenv").config();
 
 app.get("/", (req, res) => {
     res.send("Hello World");
