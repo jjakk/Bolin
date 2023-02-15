e30a5de7f59ef6dbc98813bd7f16321e2d1fe287
John Khachian <jaksawesome10@gmail.com>
2023-02-15 16:19:54 -0500

    ask button click handler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import "./App.css";
 
 function App() {
+  function ask(){
+    console.log("asking question");
+  }
+
   return (
     <div id="app">
       <header>
         <h1 id="title">Bolin</h1>
         <h3 id="subtitle">Your ChatGPT-powered personal assistant</h3>
       </header>
+      <button id="talk-button" onClick={ask}>
+        <img src="./logo.png"></img>
+      </button>
     </div>
   );
 }
