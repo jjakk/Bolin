e340d92c0fa489a519ef194f67d2d2e3b84be7e0
John Khachian <jaksawesome10@gmail.com>
2023-02-16 16:33:30 -0500

    fully works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,40 @@ import React from 'react';
 import "./App.css";
 
 function App() {
+  const SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition
+  const recognition = new SpeechRecognition();
+
   function ask(){
-    let message = prompt("What do you want to ask Bolin?");
-    fetch('http://localhost:8000/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ message })
-    })
-    .then(res => res.json())
-    .then(data => alert(data.message));
+    if(document.getElementById("talk-button").className == "activeMicrophone"){
+      recognition.stop();
+      document.getElementById("talk-button").className = "";
+    }
+    else{
+      recognition.start();
+      document.getElementById("talk-button").className = "activeMicrophone";
+    }
+
+    recognition.onspeechend = () => {
+  		recognition.stop();
+      document.getElementById("talk-button").className = "";
+  	};
+
+    recognition.onresult = async function(event) {
+  		const last = event.results.length - 1;
+  		const message = event.results[last][0].transcript;
+
+      fetch('http://localhost:8000/', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ message })
+      })
+      .then(res => res.json())
+      .then(data => {
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(data.message));
+      });
+  	};
   }
 
   return (
@@ -23,6 +46,7 @@ function App() {
       </header>
       <button id="talk-button" onClick={ask}>
         <img src="./logo.png"></img>
+        <div className="dot"></div>
       </button>
     </div>
   );
